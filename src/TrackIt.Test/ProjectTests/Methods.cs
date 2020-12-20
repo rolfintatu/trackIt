6934b6d7488cca9b2750b7c3e505418a78e8f26e
Florin Tatu <sk.florin@yahoo.com>
2020-12-20 10:26:44 +0200

    Test - Add tests for 'Project' aggregate.

---
+++ src/TrackIt.Test/ProjectTests/Methods.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using TrackIt.UI.Aggregates.Enums;
+using TrackIt.UI.Aggregates.ProjectAggregate;
+
+namespace TrackIt.Test.ProjectTests
+{
+    [TestClass]
+    public class Methods
+    {
+
+        private Project fakeProject { get; set; }
+
+        public Methods()
+        {
+            fakeProject = Project.CreateInstance("Test Project", "A fake project for testing.");
+        }
+
+        [TestMethod]
+        public void add_ticket()
+        {
+            fakeProject.CreateTicket("First ticket", "A fake ticket for this project.", null);
+
+            Assert.AreEqual(1, fakeProject.Tickets.Count());
+        }
+
+        [TestMethod]
+        public void close_ticket()
+        {
+            fakeProject.CreateTicket("First ticket", "A fake ticket for this project.", null);
+
+            Ticket fakeTicket = fakeProject.Tickets.First();
+
+            fakeProject.CloseTicket(fakeTicket.Id);
+
+            Assert.AreEqual(TicketState.Close, fakeTicket.TicketState);
+        }
+
+        [TestMethod]
+        public void assign_new_worker()
+        {
+            fakeProject.AssignNewWorker(Guid.NewGuid().ToString());
+
+            Assert.AreEqual(1, fakeProject.Workers.Count());
+        }
+
+        [TestMethod]
+        public void dismiss_worker()
+        {
+            fakeProject.AssignNewWorker(Guid.NewGuid().ToString());
+
+            Worker fakeWorker = fakeProject.Workers.First();
+
+            fakeProject.DismissWorker(fakeWorker.Id);
+
+            Assert.AreEqual(0, fakeProject.Workers.Count());
+        }
+
+        [TestMethod]
+        public void workers_changed()
+        {
+            fakeProject.AssignNewWorker(Guid.NewGuid().ToString());
+
+            Assert.IsTrue(fakeProject.workersChange);
+        }
+
+        [TestMethod]
+        public void tickets_changed()
+        {
+            fakeProject.CreateTicket("First ticket", "A fake ticket for this project.", null);
+
+            Assert.IsTrue(fakeProject.ticketsChagne);
+        }
+    }
+}
---
+++ src/TrackIt.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrackIt.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrackIt.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0851ba05-8ea3-469f-9327-551b558c32df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
