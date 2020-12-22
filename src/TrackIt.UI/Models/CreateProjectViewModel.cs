e413254fde9434794cd200f5da311c046c331531
Florin Tatu <sk.florin@yahoo.com>
2020-12-22 11:41:18 +0200

    Models - Created models for 'Project' views.

---
+++ src/TrackIt.UI/Models/CreateProjectViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackIt.UI.Models
+{
+    public class CreateProjectViewModel
+    {
+
+        public CreateProjectViewModel()
+        {
+            Workers = new Dictionary<ApplicationUser, bool>();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Dictionary<ApplicationUser, bool> Workers { get; set; }
+    }
+}
---
+++ src/TrackIt.UI/Models/ProjectDetailsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackIt.UI.Models
+{
+    public class ProjectDetailsViewModel
+    {
+
+        public ProjectDetailsViewModel()
+        {
+            Workers = new List<ApplicationUser>();
+            Tickets = new List<TicketModel>();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<ApplicationUser> Workers { get; set; }
+        public List<TicketModel> Tickets { get; set; }
+
+    }
+
+
+    public class TicketModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
